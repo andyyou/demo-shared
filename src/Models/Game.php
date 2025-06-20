553db2de7c4eed55f1de1e787dc16ccbf767ddb4
andyyou <andyyu0920@gmail.com>
2025-06-20 11:37:59 +0800

    fix Game 持續變更 3 from demo-dash

--- src/Models/Game.php
+++ src/Models/Game.php
@@ -4,12 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Game extends Model
 {
     use SoftDeletes;
+    use HasFactory;
 
     protected $fillable = [];
 
+
+    public function game_type()
+    {
+        return $this->belongsTo(GameType::class);
+    }
     
 }
